908258ce89bc56f4dd270c3125342f2c0c874090
Cody M Beardsley <beardsley.cody@gmail.com>
2014-04-22 15:09:06 -0700

    Modifications to advanced search

--- wp-content/plugins/bp-profile-search/bps-admin.js
+++ wp-content/plugins/bp-profile-search/bps-admin.js
@@ -64,7 +64,7 @@ function add_field(forWhat) {
 	});
 
 	var labelMemberType = document.createElement( 'label' );
-	labelMemberType.appendChild( member_type );
+	labelMemberType.appendChild( memberType );
 	labelMemberType.appendChild( document.createTextNode( bps_strings.member_type + ' ' ) );
 
 	var toDelete = document.createElement( 'a' );
